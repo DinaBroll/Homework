60d066bc7e2bc4eb2c6380ee459ee238991822da
DinaBroll <smiffnaj.mifka@gmail.com>
2020-02-09 15:40:18 +0300

    Homework of the 3 and 4 lesson

---
+++ Lesson 4/Task1.js
@@ -0,0 +1,15 @@
+function pow(x, n) {
+  var res = x;
+  for (var i = 1; i < n; i++) {
+    res *= x;
+  }
+  return res;
+}
+var x = parseInt(prompt("Введите число", ""));
+var n = parseInt(prompt("В какую степень возвести?", ""));
+if (x > 0 && n > 0) {
+  alert(pow(x, n));
+} else {
+  alert("Введите положительное число");
+}
+
