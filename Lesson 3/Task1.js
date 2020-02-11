03890f26e28b2f4754969a78d79740eee5fcd508
DinaBroll <smiffnaj.mifka@gmail.com>
2020-02-11 20:54:15 +0300

    Homework of the 3 and 4 lesson

--- Lesson 3/Task1.js
+++ Lesson 3/Task1.js
@@ -1,5 +1,6 @@
-var styles = ["Джаз", "Блюз"];
-styles.push("Рок-н-Ролл");
-styles[styles.length - 2] = "Классика";
+var styles = ['Джаз', 'Блюз'];
+styles.push('Рок-н-Ролл');
+styles[styles.length - 2] = 'Классика';
 styles.shift();
-styles.unshift("Рэп", "Регги");
+styles.unshift('Рэп', 'Регги');
+console.log(styles);
--- Lesson 3/task4.js
+++ Lesson 3/task4.js
@@ -1,8 +1,8 @@
 var arr = [];
 for (var i = 0; ; i++) {
-  var question = prompt("Введите число");
+  var question = prompt('Введите число');
 
-  if (question === "" || question === null || isNaN(question)) {
+  if (question === '' || question === null || isNaN(question)) {
     break;
   }
   arr.push(+question);
@@ -11,5 +11,5 @@ var summ = 0;
 for (var j = 0; j < arr.length; j++) {
   summ += arr[j];
 }
-alert("Сумма всех значений массива = " + summ);
+alert('Сумма всех значений массива = ' + summ);
 console.log(arr);
