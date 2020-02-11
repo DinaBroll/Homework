4f75d54eba7f6cce451532db1af6305660e5c20c
DinaBroll <smiffnaj.mifka@gmail.com>
2020-02-11 19:03:25 +0300

    Homework of the 3 and 4 lesson

--- Lesson 3/Task1.js
+++ Lesson 3/Task1.js
@@ -1,5 +1,5 @@
 var styles = ["Джаз", "Блюз"];
 styles.push("Рок-н-Ролл");
-styles.splice(styles.length - 2, 1, "Классика");
+styles[styles.length - 2] = "Классика";
 styles.shift();
 styles.unshift("Рэп", "Регги");
