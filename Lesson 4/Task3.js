4c735283a8f3191cc3e6bf8aa7040d49037b5652
DinaBroll <smiffnaj.mifka@gmail.com>
2020-02-12 19:38:59 +0300

    Homework of the 3 and 4 lesson

--- Lesson 4/Task3.js
+++ Lesson 4/Task3.js
@@ -2,7 +2,7 @@ var arr = [5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8];
 function treeSum(arr) {
   var result = 0;
   for (var i = 0; i < arr.length; i++) {
-    if (typeof arr[i] == 'number') {
+    if (typeof arr[i] == 'number' && !(isNaN(arr[i]))) {
       result += +arr[i];
     } else if (typeof arr == 'object' && i < arr.length) {
       result += treeSum(arr[i]);
