b7248978677d9b329782a7f1d07daf22ff04de25
DinaBroll <smiffnaj.mifka@gmail.com>
2020-03-30 21:37:18 +0300

    homework lesson 13

--- Lesson 12/task 2.js
+++ Lesson 12/task 2.js
@@ -1,6 +1,6 @@
 var strs = ['00', '13', '24'];
-var result = strs.reduce(function(phrase, numbes, index) {
-  return index === 0
-    ? 'Текущее время: ' + phrase + numbes
-    : phrase + ' : ' + numbes;
-}, '');
+var result = strs.reduce(function(phrase, number) {
+  return phrase + ' : ' + number;
+}, 'Текущее время');
+
+
